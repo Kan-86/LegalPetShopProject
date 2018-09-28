f82be66df377ebf594ffaf21a040671ce816ee2b
Sub-Z3r0 <andersen19@gmail.com>
2018-09-28 11:26:53 +0200

    yes

---
+++ PetApp.Core.Entity/Colours.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetApp.Core.Entity
+{
+    public class Colours
+    {
+        public int ColourId { get; set; }
+        public PetColour PetColour { get; set; }
+
+        public int PetId { get; set; }
+        public Pet Pet { get; set; }
+    }
+}
---
+++ PetApp.Core.Entity/PetColour.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetApp.Core.Entity
+{
+    public class PetColour
+    {
+        /*public int Id { get; set; }
+        public string PetColour { get; set; }
+        public List<Colours> Colourss { get; set; }*/
+    }
+}
